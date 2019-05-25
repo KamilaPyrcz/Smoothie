00a0307da843de297d0f5bb26183c2cea4ed9434
KamilaPyrcz <pyrcz.kamila@gmail.com>
2019-05-25 21:34:22 +0200

    gulp fix, header changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,26 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
+const wait = require('gulp-wait');
 
 gulp.task('sass', function () {
 	return gulp.src('app/assets/stylesheets/main.scss')
-	// .pipe(wait(200))
+	.pipe(wait(200))
 	.pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}, {errLogToConsole: true}))
 	.pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
 	.pipe(gulp.dest('./app/assets/css'))
 	.pipe(browserSync.stream())
 });
 
-gulp.task('imagemin', function() {
-	return gulp .src('app/assets/img/*')
-	.pipe(imagemin()) 
-	.pipe(gulp.dest('.app/assets/stylesheets/minified/images'));
-});
-
-gulp.task('serve', gulp.series('sass', 'imagemin', function () {
+gulp.task('serve', gulp.series('sass', function () {
 	browserSync.init({
 		server: {
 			baseDir:'./app'
 		}
 	});
-	gulp.watch("app/scss/*.scss",  gulp.series('sass'));
-	gulp.watch('app/assets/img/*', gulp.series('imagemin'));
-	gulp.watch('app/*.html').on('change', browserSync.reload);
-	gulp.watch('app/*.js').on('change', browserSync.reload);
+	gulp.watch('app/assets/stylesheets/_*.scss',  gulp.series('sass')).on('change', browserSync.reload)
+	gulp.watch('app/*.html').on('change', browserSync.reload)
+	gulp.watch('app/*.js').on('change', browserSync.reload)
 }));
 
 gulp.task('default', gulp.series('serve'));
